09fbe51ea6d0e820c3644083985a54542f10ccae
AL KAISI <ibrahim.al-kaisi@univ-rouen.fr>
2023-09-07 16:37:15 +0200

    Ajout d'autres tests y compris les cas limites à test.py

--- TP1/test.py
+++ TP1/test.py
@@ -6,5 +6,30 @@ def test_1():
     assert f.puissance(2, 2) == 4
 
 def test_2():
-	assert f.puissance(-3, 3) == -27
-	assert f.puissance(-2, 2) == 4
+    # Test avec des valeurs négatives et des exposants négatifs
+    assert f.puissance(-2, 2) == 4
+    assert f.puissance(-2, -3) == -0.125
+    
+    # Test avec des valeurs négatives et des exposants positifs
+    assert f.puissance(-3, 3) == -27
+    assert f.puissance(-4, 5) == -1024
+    
+    # Test avec des valeurs positives et des exposants négatifs
+    assert f.puissance(5, -2) == 1/25
+    assert f.puissance(2, -4) == 1/16
+    
+    # Cas limite : Exposant nul
+    assert f.puissance(10, 0) == 1
+    
+    # Cas limite : Base nulle, exposant positif
+    assert f.puissance(0, 5) == 0
+    
+    # Cas limite : Base nulle, exposant nul
+    assert f.puissance(0, 0) == 1
+    
+    # Cas limite : Base négative, exposant pair
+    assert f.puissance(-2, 4) == 16
+    
+    # Cas limite : Base négative, exposant impair
+    assert f.puissance(-2, 3) == -8
+
